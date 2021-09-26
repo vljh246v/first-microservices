4c74071178c4f12fac6cbb5ab78da67d7769a94e
vljh246v <vljh246v@naver.com>
2021-09-26 20:11:28 +0900

    modify get product logic

--- microservices/product-service/src/main/java/me/demo/microservices/core/product/services/ProductServiceImpl.java
+++ microservices/product-service/src/main/java/me/demo/microservices/core/product/services/ProductServiceImpl.java
@@ -2,12 +2,18 @@ package me.demo.microservices.core.product.services;
 
 import me.demo.api.core.product.Product;
 import me.demo.api.core.product.ProductService;
+import me.demo.util.exceptions.InvalidInputException;
+import me.demo.util.exceptions.NotFoundException;
 import me.demo.util.http.ServiceUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductServiceImpl implements ProductService {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
 
   private final ServiceUtil serviceUtil;
 
@@ -19,6 +25,12 @@ public class ProductServiceImpl implements ProductService {
 
   @Override
   public Product getProduct(int productId) {
+    LOG.debug("/product return the found product for productId={}", productId);
+
+    if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
+
+    if (productId == 13) throw new NotFoundException("No product found for productId: " + productId);
+
     return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
   }
 }
