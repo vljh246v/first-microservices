918b1b3ddb3ab606cf8881d67e8bb4d725cd1299
vljh246v <vljh246v@naver.com>
2021-09-26 17:50:47 +0900

    add Integration Composite

---
+++ microservices/product-composite-service/src/main/java/me/demo/microservices/composite/product/service/ProductCompositeIntegration.java
@@ -0,0 +1,76 @@
+package me.demo.microservices.composite.product.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.demo.api.core.product.Product;
+import me.demo.api.core.product.ProductService;
+import me.demo.api.core.recommendation.Recommendation;
+import me.demo.api.core.recommendation.RecommendationService;
+import me.demo.api.core.review.Review;
+import me.demo.api.core.review.ReviewService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Component
+public class ProductCompositeIntegration implements ProductService, RecommendationService, ReviewService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProductCompositeIntegration.class);
+
+    private final RestTemplate restTemplate;
+    private final ObjectMapper mapper;
+
+    private final String productServiceUrl;
+    private final String recommendationServiceUrl;
+    private final String reviewServiceUrl;
+
+    @Autowired
+    public ProductCompositeIntegration(
+            RestTemplate restTemplate,
+            ObjectMapper mapper,
+
+            @Value("${app.product-service.host}") String productServiceHost,
+            @Value("${app.product-service.port}") int    productServicePort,
+
+            @Value("${app.recommendation-service.host}") String recommendationServiceHost,
+            @Value("${app.recommendation-service.port}") int    recommendationServicePort,
+
+            @Value("${app.review-service.host}") String reviewServiceHost,
+            @Value("${app.review-service.port}") int    reviewServicePort
+    ) {
+
+        this.restTemplate = restTemplate;
+        this.mapper = mapper;
+
+        productServiceUrl        = "http://" + productServiceHost + ":" + productServicePort + "/product/";
+        recommendationServiceUrl = "http://" + recommendationServiceHost + ":" + recommendationServicePort + "/recommendation?productId=";
+        reviewServiceUrl         = "http://" + reviewServiceHost + ":" + reviewServicePort + "/review?productId=";
+    }
+
+    @Override
+    public Product getProduct(int productId) {
+        String url = productServiceUrl + productId;
+        Product product = restTemplate.getForObject(url, Product.class);
+        return product;
+    }
+
+    @Override
+    public List<Recommendation> getRecommendations(int productId) {
+        String url = recommendationServiceUrl + productId;
+        List<Recommendation> recommendations = restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<Recommendation>>() {}).getBody();
+        return recommendations;
+    }
+
+    @Override
+    public List<Review> getReviews(int productId) {
+        String url = reviewServiceUrl + productId;
+        List<Review> reviews = restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<Review>>() {}).getBody();
+        return reviews;
+    }
+}
