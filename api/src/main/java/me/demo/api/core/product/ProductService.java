a71c4c7f5bb70e74ee366bc2d7ef1bfb6f8d6404
vljh246v <vljh246v@naver.com>
2021-09-26 17:34:12 +0900

    ProductService javadoc 주석 추가

--- api/src/main/java/me/demo/api/core/product/ProductService.java
+++ api/src/main/java/me/demo/api/core/product/ProductService.java
@@ -4,6 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 public interface ProductService {
+
+  /**
+   * Sample usage: curl $HOST:$PORT/product/1
+   *
+   * @param productId
+   * @return the product, if found, else null
+   */
   @GetMapping(
       value = "/product/{productId}",
       produces = "application/json"
