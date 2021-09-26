9ddf543a63e6356445b84b9fc1306baa28342672
vljh246v <vljh246v@naver.com>
2021-09-26 20:07:49 +0900

    add  ComponentScan package

--- microservices/recommendation-service/src/main/java/me/demo/microservices/core/recommendation/RecommendationServiceApplication.java
+++ microservices/recommendation-service/src/main/java/me/demo/microservices/core/recommendation/RecommendationServiceApplication.java
@@ -2,8 +2,10 @@ package me.demo.microservices.core.recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("me.demo")
 public class RecommendationServiceApplication {
 
 	public static void main(String[] args) {
--- microservices/review-service/src/main/java/me/demo/microservices/core/review/ReviewServiceApplication.java
+++ microservices/review-service/src/main/java/me/demo/microservices/core/review/ReviewServiceApplication.java
@@ -2,8 +2,10 @@ package me.demo.microservices.core.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("me.demo")
 public class ReviewServiceApplication {
 
 	public static void main(String[] args) {
